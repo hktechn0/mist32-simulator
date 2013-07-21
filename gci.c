39a366d15e0da0247af990397c1b18d2c21c4f5a
Hirotaka Kawata <hirotaka@techno-st.net>
2013-07-22 05:27:19 +0900

    bug fixed: GCI-MMCC addresses is not LBA

--- gci.c
+++ gci.c
@@ -282,23 +282,23 @@ void gci_mmcc_write(Memory addr, Memory offset, void *mem)
     DEBUGIO("[I/O] MMCC INIT_COMMAND\n");
   }
   else if(offset == GCI_MMCC_SECTOR_READ) {
-    if(lseek(fd_mmcc, mmcc->sector_read * MMCC_SECTOR_SIZE, SEEK_SET) == -1) {
+    if(lseek(fd_mmcc, mmcc->sector_read, SEEK_SET) == -1) {
       errx(EXIT_FAILURE, "MMCC READ lseek");
     }
     if(read(fd_mmcc, buf, MMCC_SECTOR_SIZE) == -1) {
       errx(EXIT_FAILURE, "MMCC READ read");
     }
 
-    DEBUGIO("[I/O] MMCC READ Sector: %d\n", mmcc->sector_read);
+    DEBUGIO("[I/O] MMCC READ Sector: %d\n", mmcc->sector_read >> 9);
   }
   else if(offset == GCI_MMCC_SECTOR_WRITE) {
-    if(lseek(fd_mmcc, mmcc->sector_write * MMCC_SECTOR_SIZE, SEEK_SET) == -1) {
+    if(lseek(fd_mmcc, mmcc->sector_write, SEEK_SET) == -1) {
       errx(EXIT_FAILURE, "MMCC WRITE lseek");
     }
     if(write(fd_mmcc, buf, MMCC_SECTOR_SIZE) == -1) {
       errx(EXIT_FAILURE, "MMCC WRITE write");
     }
 
-    DEBUGIO("[I/O] MMCC WRITE Sector: %d\n", mmcc->sector_write);
+    DEBUGIO("[I/O] MMCC WRITE Sector: %d\n", mmcc->sector_write >> 9);
   }
 }
